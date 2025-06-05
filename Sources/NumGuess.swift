0818259c6eb91f03ca307876c61f103621c0e71f
Drew Williams <DrewW@iARx.com>
2025-06-05 11:54:13 -0400

    more swifty

--- Sources/NumGuess.swift
+++ Sources/NumGuess.swift
@@ -7,35 +7,32 @@ struct NumGuess {
         repeat {
             print("What is your guess? : ", terminator: "")
 
-            if let input = readLine() {
-                if let number = Int(input) {
-                    guess = number
-                }
-                else {
-                    print("That is not a number silly. Try again!")
-                    continue
-                }
-            }
-            else {
+            guard let input = readLine() else {
                 print("Oh no! I think something went wrong. :( Im gonna stop now")
                 break
             }
 
-            if(guess > 100 || guess < 1) {
+            guard let number = Int(input) else {
+                print("That is not a number silly. Try again!")
+                continue
+            }
+
+            guess = number
+
+            guard 1...100 ~= guess else {
                 print("That number is outside the range! Try again!")
                 continue
             }
 
-            if(guess > answer) {
+            if guess > answer {
                 print("That is too high. Try again!")
-            }
-            if(guess < answer) {
+            } else if guess < answer {
                 print("That is too low. Try again!")
             }
         }
-        while(answer != guess)
+        while answer != guess
 
-        if(answer == guess) {
+        if answer == guess {
             print("Yay! You win!")
         }
     }
